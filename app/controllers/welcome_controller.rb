aecc7c0af1636e33117206f52a203638e07f9331
hemingsha <shm@djcard.cn>
2018-03-08 13:13:37 +0800

    Fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,31 @@
 class WelcomeController < ApplicationController
   before_action :check_1, :check_2, :only => :test
 
-
-
   def index
     @text = Time.now.to_s + "SHAME"
-    logger.info {"=====#{Time.now.to_s}======="}
+
+    if current_user
+      a = Mechanize.new
+      a.get('https://www.worldcryptocap.com') do |page|
+        # Click the login link
+
+        # Submit the login form
+        my_page = page.form_with(:action => '/') do |f|
+          f.fields[1].value = "sander123"
+          f.fields[2].value = "worldcryptocap_sha_1234"
+        end.click_button
+
+        wcg_price = my_page.search("#shares")[-1].children[-2].children[3].children[1].children.text
+        @text = "WCG: $#{wcg_price.strip}"
+      end
+    end
+    logger.info { "=====#{Time.now.to_s}=======" }
   end
 
   def test
     render :json => {msg: "hello word! test  ww"}
     p params
   end
-
 
   def check_1
     render :json => {msg: "data less than 1"} if params[:data].to_i <= 1
